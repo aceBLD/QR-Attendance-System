80d59e92815f1cc80f8a84ef45d6f4aa7aa7a376
ÆCE <Beldadj48@gmail.com>
2025-08-24 07:10:52 +0000

    modify and add new files bruh

---
+++ Scripts/mainUnit.js
@@ -0,0 +1,8 @@
+let SignIn = Document.getElementById("SignIn");
+let SignUp = Document.getElementById("SignUp");
+let mainUnit = Document.getElementById("theunit");
+
+function signIn_studentOrTeacher() {
+    mainUnit.style.display = "none";
+
+}
