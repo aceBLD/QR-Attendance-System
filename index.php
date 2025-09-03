05bf60e704a68d5f74f14fc9e8e759c0918a4069
ÆCE <Beldadj48@gmail.com>
2025-09-03 12:07:02 +0000

    few CHanges for index lol

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // To run the website using PHP language by locating the mainpage
 //source code by ACE | Miku aka Mikuhimono on their PR2 tool
-header("Location: WebShita/main.html");
+header("Location: WebShita/index.html");
 exit;
 ?>
