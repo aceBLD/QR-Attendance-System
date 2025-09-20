b22395298ba24758f117c8ca29d7afb627e17455
ÆCE <Beldadj48@gmail.com>
2025-09-20 20:17:36 +0800

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// To run the website using PHP language by locating the mainpage
-//source code by ACE | Miku aka Mikuhimono on their PR2 tool
-header("Location: WebShita/index.html");
-exit;
-?>
